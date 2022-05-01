5b90076ffd43a5804197dbd803cdb124682e5d17
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-05-01 09:42:24 +0800

    Update emby.sh

--- install/emby.sh
+++ install/emby.sh
@@ -14,8 +14,8 @@ github_url="https://github.com/johnrosen1/vpstoolbox"
 install_emby(){
 apt install -y apt-transport-https
 curl --retry 5 -LO https://github.com/MediaBrowser/Emby.Releases/releases/download/4.6.7.0/emby-server-deb_4.6.7.0_amd64.deb
-dpkg -i emby-server-deb_4.6.7.0_amd64.deb
-rm emby-server-deb_4.6.7.0_amd64.deb
+dpkg -i emby-server*.deb
+rm emby-server*.deb
 systemctl stop emby-server
 sed -i "s/EMBY_DATA=/EMBY_DATA=\/data\/media\//g" /etc/emby-server.conf
 sed -i "s/User=emby/User=root/g" /lib/systemd/system/emby-server.service

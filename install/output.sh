18a190d5adf535bfed90d6ab3c01b0e6caa34f68
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-05-09 20:52:06 +0800

    Update output.sh

--- install/output.sh
+++ install/output.sh
@@ -43,6 +43,9 @@ echo -e " --- \${BLUE}服務狀態(Service Status)\${NOCOLOR} ---"
   if [[ \$(cat /etc/sysctl.conf | grep bbr) = *bbr* ]] ; then
 echo -e "BBR网络优化:\t\t 已开启"
   fi
+  if [[ \$(systemctl is-active wg-quick@wgcf.service) == active ]]; then
+echo -e "Warp+ Teams:\t\t 正常运行中"
+  fi
   if [[ \$(systemctl is-active trojan) == active ]]; then
 echo -e "Trojan-GFW:\t\t 正常运行中"
   fi

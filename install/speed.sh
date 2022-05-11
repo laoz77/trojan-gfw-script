a379f09f0302fd70bea851e08443f467038ad1bb
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-05-11 10:36:52 +0800

    Update speed.sh

--- install/speed.sh
+++ install/speed.sh
@@ -12,6 +12,7 @@ github_url="https://github.com/johnrosen1/vpstoolbox"
 set +e
 
 install_speed(){
+# https://github.com/sivel/speedtest-cli
 apt install pip -y
 pip install speedtest-cli
 TERM=ansi whiptail --title "带宽测试" --infobox "带宽测试，请耐心等待。" 7 68

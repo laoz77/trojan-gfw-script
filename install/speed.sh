cb7fdcca24a38bccfb89d2bac7faf14694bd8d0d
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-05-11 12:41:25 +0800

    Update speed.sh

--- install/speed.sh
+++ install/speed.sh
@@ -18,4 +18,5 @@ pip install speedtest-cli
 TERM=ansi whiptail --title "带宽测试" --infobox "带宽测试，请耐心等待。" 7 68
 speedtest-cli --list
 speedtest-cli --secure | tee /root/.trojan/speed.txt
+speedtest-cli --secure --json | tee /root/.trojan/speed.json
 }

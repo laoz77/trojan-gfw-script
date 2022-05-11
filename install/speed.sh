ba82de659c573c610724425349a19f9b203aa9c6
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-05-11 23:06:16 +0800

    update speed module

--- install/speed.sh
+++ install/speed.sh
@@ -15,36 +15,21 @@ mycountry="$( jq -r '.country' "/root/.trojan/ip.json" )"
 
 install_speed(){
 # https://github.com/sivel/speedtest-cli
-apt install pip -y
-pip install speedtest-cli
+curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+apt-get install speedtest -y
 TERM=ansi whiptail --title "带宽测试" --infobox "带宽测试，请耐心等待。" 7 68
-speedtest-cli --list
-speedtest-cli --secure --no-upload --json | tee /root/.trojan/speed_ping.json
-ping_result=$( jq -r '.ping' "/root/.trojan/speed_ping.json" )
-
-if (( $(echo "$ping_result < 10" |bc -l) )); then
-speedtest-cli --secure | tee /root/.trojan/speed.txt
-speedtest-cli --secure --json | tee /root/.trojan/speed.json
-elif [[ ${mycountry} == "JP" ]]
-# speedtest-cli --secure --server 48296 | tee /root/.trojan/speed.txt
-# speedtest-cli --secure --server 48296 --json | tee /root/.trojan/speed.json
-speedtest-cli --secure | tee /root/.trojan/speed.txt
-speedtest-cli --secure --json | tee /root/.trojan/speed.json
-else
-speedtest-cli --secure | tee /root/.trojan/speed.txt
-speedtest-cli --secure --json | tee /root/.trojan/speed.json
-fi
+echo "YES" | /usr/bin/speedtest | tee /root/.trojan/speed.txt
+/usr/bin/speedtest -f json | tee /root/.trojan/speed.json
+port_down=$( jq -r '.download.bandwidth' "/root/.trojan/speed.json" )
+port_up=$( jq -r '.upload.bandwidth' "/root/.trojan/speed.json" )
 
 apt install bc -y
-
-port_down=$( jq -r '.download' "/root/.trojan/speed.json" )
-port_up=$( jq -r '.upload' "/root/.trojan/speed.json" )
 
 if (( $(echo "$port_up < 100000000" |bc -l) )); then
 target_speed_down="100"
 target_speed_up="100"
 else
-target_speed_down=$( jq -r '.download' "/root/.trojan/speed.json" | cut -c1-3)
-target_speed_up=$( jq -r '.upload' "/root/.trojan/speed.json" | cut -c1-3)
+target_speed_down=$( jq -r '.download.bandwidth' "/root/.trojan/speed.json" | cut -c1-3)
+target_speed_up=$( jq -r '.upload.bandwidth' "/root/.trojan/speed.json" | cut -c1-3)
 fi
 }

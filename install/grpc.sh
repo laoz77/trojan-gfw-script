cda8cc5c25324aa5d96cf17269c4866ee51fd3d8
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-05-13 17:32:58 +0800

    Update grpc.sh

--- install/grpc.sh
+++ install/grpc.sh
@@ -159,7 +159,7 @@ systemctl restart grpc.service
 
 cd /root
 
-## 给 Vless Tor网络访问权限
+## 给 Vless Tor 网络访问权限
 curl --retry 5 -LO https://raw.githubusercontent.com/johnrosen1/vpstoolbox/master/install/tor.sh
 source tor.sh
 install_tor

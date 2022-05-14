b6390a3db37f4a82d935cd1f3c5ea33e471d87ff
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-05-14 18:26:06 +0800

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -90,22 +90,22 @@ setlanguage(){
   chattr -i /etc/locale.gen
   cat > '/etc/locale.gen' << EOF
 C.UTF-8 UTF-8
-zh_CN.UTF-8 UTF-8
-zh_TW.UTF-8 UTF-8
-en_US.UTF-8 UTF-8
-ja_JP.UTF-8 UTF-8
+#zh_CN.UTF-8 UTF-8
+#zh_TW.UTF-8 UTF-8
+#en_US.UTF-8 UTF-8
+#ja_JP.UTF-8 UTF-8
 EOF
 locale-gen
 update-locale
 chattr -i /etc/default/locale
   cat > '/etc/default/locale' << EOF
-LANGUAGE="zh_CN.UTF-8"
-LANG="zh_CN.UTF-8"
-LC_ALL="zh_CN.UTF-8"
+LANGUAGE="C.UTF-8"
+LANG="C.UTF-8"
+LC_ALL="C.UTF-8"
 EOF
-export LANGUAGE="zh_CN.UTF-8"
-export LANG="zh_CN.UTF-8"
-export LC_ALL="zh_CN.UTF-8"
+export LANGUAGE="C.UTF-8"
+export LANG="C.UTF-8"
+export LC_ALL="C.UTF-8"
 }
 
 ## 写入配置文件

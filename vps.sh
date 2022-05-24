f35aaa953d4c901da84605ef6d22d60dc7b039a3
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-05-24 13:30:18 +0800

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -382,9 +382,7 @@ clear
 ## 安装具体软件
 install_moudles(){
   # Src url : https://github.com/johnrosen1/vpstoolbox/blob/master/install/
-  curl --retry 5 -LO https://raw.githubusercontent.com/johnrosen1/vpstoolbox/master/install/bbr.sh
-  source bbr.sh
-  install_bbr
+
   if [[ ${install_docker} == 1 ]]; then
   curl --retry 5 -LO https://raw.githubusercontent.com/johnrosen1/vpstoolbox/master/install/docker.sh
   source docker.sh
@@ -555,6 +553,9 @@ MasterMenu() {
     source detectcert.sh
     detectcert
     ## 开始安装
+    curl --retry 5 -LO https://raw.githubusercontent.com/johnrosen1/vpstoolbox/master/install/bbr.sh
+    source bbr.sh
+    install_bbr
     TERM=ansi whiptail --title "开始安装" --infobox "安装开始,请不要按任何按键直到安装完成(Please do not press any button until the installation is completed)!" 7 68
     colorEcho ${INFO} "安装开始,请不要按任何按键直到安装完成(Please do not press any button until the installation is completed)!"
     curl --retry 5 -LO https://raw.githubusercontent.com/johnrosen1/vpstoolbox/master/install/system-upgrade.sh

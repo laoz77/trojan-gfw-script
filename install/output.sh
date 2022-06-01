0eed42e3374825ae567a00dc7edd34962bfe73ec
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-06-01 16:57:10 +0800

    enable auto apt update

--- install/output.sh
+++ install/output.sh
@@ -8,6 +8,11 @@ prase_output(){
 
 apt-get install unattended-upgrades -y
 
+  cat > '/etc/apt/apt.conf.d/20auto-upgrades' << EOF
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Unattended-Upgrade "1";
+EOF
+
 ## Cloudflare Warp For IPv4 only server
 
 if [[ -z ${myipv6} ]] || [[ ${myipv6} =~ .*"fd01".* ]]; then

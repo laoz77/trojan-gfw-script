0b3d6551fb6b2fbae2f57d3f170e84e0091142db
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-06-04 17:39:50 +0800

    update bbr config

--- install/bbr.sh
+++ install/bbr.sh
@@ -95,7 +95,7 @@ net.netfilter.nf_conntrack_tcp_timeout_time_wait = 60
 net.netfilter.nf_conntrack_tcp_timeout_close_wait = 30
 net.netfilter.nf_conntrack_tcp_timeout_established = 86400
 ## Enable bbr
-net.core.default_qdisc = fq
+net.core.default_qdisc = cake
 net.ipv4.tcp_congestion_control = bbr
 net.ipv4.tcp_low_latency = 1
 fs.inotify.max_user_instances=8192

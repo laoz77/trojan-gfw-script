2bd6bebf9c9edcbeefa74d9d3b0057288b512e7f
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-06-06 18:07:54 +0800

    update cert status output

--- install/output.sh
+++ install/output.sh
@@ -168,7 +168,7 @@ echo -e " --- \${BLUE}带宽使用(Bandwith Usage)\${NOCOLOR} ---"
 echo -e "         接收(Receive)    发送(Transmit)"
 tail -n +3 /proc/net/dev | grep -e eth -e enp -e eno -e ens | awk '{print \$1 " " \$2 " " \$10}' | numfmt --to=iec --field=2,3
 echo -e " --- \${GREEN}證書狀態(Certificate Status)\${NOCOLOR} ---"
-ssl_date=\$(echo |timeout 3 openssl s_client -4 -connect ${myip}:${trojanport} -servername ${domain} -tls1_3 2>&1 |sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'|openssl x509 -text) &>/dev/null
+ssl_date=\$(openssl x509 -in /etc/certs/${domain}_ecc/fullchain.cer -text -noout) &>/dev/null
 tmp_last_date=\$(echo "\${ssl_date}" | grep 'Not After :' | awk -F' : ' '{print \$NF}')
 last_date=\$(date -ud "\${tmp_last_date}" +%Y-%m-%d" "%H:%M:%S)
 day_count=\$(( (\$(date -d "\${last_date}" +%s) - \$(date +%s))/(24*60*60) ))

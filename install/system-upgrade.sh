015c91fe8ecc5a4a549d05fdd1c441b417605262
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-06-20 21:47:47 +0800

    Update system-upgrade.sh

--- install/system-upgrade.sh
+++ install/system-upgrade.sh
@@ -8,19 +8,19 @@ upgrade_system(){
 
 ## Install lastest ubuntu lts
 if [[ $dist == ubuntu ]]; then
-    cat > '/etc/apt/sources.list' << EOF
-deb http://archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse
-deb http://archive.ubuntu.com/ubuntu/ focal-updates main restricted universe multiverse
-deb http://archive.ubuntu.com/ubuntu/ focal-security main restricted universe multiverse
-deb http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse
-deb http://archive.canonical.com/ubuntu focal partner
-EOF
-  apt-get update --fix-missing
-  sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt-get upgrade -q -y'
-  sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -q -y'
-  sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt --fix-broken install -q -y'
-  sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt-get autoremove -q -y'
-  clear
+#     cat > '/etc/apt/sources.list' << EOF
+# deb http://archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse
+# deb http://archive.ubuntu.com/ubuntu/ focal-updates main restricted universe multiverse
+# deb http://archive.ubuntu.com/ubuntu/ focal-security main restricted universe multiverse
+# deb http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse
+# deb http://archive.canonical.com/ubuntu focal partner
+# EOF
+#   apt-get update --fix-missing
+#   sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt-get upgrade -q -y'
+#   sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -q -y'
+#   sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt --fix-broken install -q -y'
+#   sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt-get autoremove -q -y'
+#   clear
 else
 ## Install lastest stable debian
   apt-get update --fix-missing

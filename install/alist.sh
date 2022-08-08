ff6ea38a675c2b3f859ce96f4c00d2b1a896b0a5
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-08-08 22:29:25 +0800

    Update alist.sh

--- install/alist.sh
+++ install/alist.sh
@@ -14,5 +14,27 @@ install_alist(){
 cat '/opt/alist/data/config.json' | jq '.address |= "127.0.0.1"' >> /opt/alist/data/config.json.tmp
 cp -f /opt/alist/data/config.json.tmp /opt/alist/data/config.json
 rm /opt/alist/data/config.json.tmp
+
+  cat > '/etc/systemd/system/alist.service' << EOF
+[Unit]
+Description=Alist service
+Wants=network.target
+After=network.target network.service
+
+[Service]
+Type=simple
+WorkingDirectory=/opt/alist
+ExecStart=/opt/alist/alist
+KillMode=process
+LimitNOFILE=infinity
+RestartSec=3s
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable alist
 systemctl restart alist
 }

2945609c740eb1e90c8620654c509f57df3745b6
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-08-13 23:01:26 +0800

    Update nginx-config.sh

--- install/nginx-config.sh
+++ install/nginx-config.sh
@@ -72,6 +72,8 @@ echo "    #access_log off;" >> /etc/nginx/conf.d/default.conf
 echo "    client_max_body_size 0;" >> /etc/nginx/conf.d/default.conf
 echo "    proxy_set_header X-Forwarded-Proto https;" >> /etc/nginx/conf.d/default.conf
 echo "    proxy_pass http://127.0.0.1:5244/;" >> /etc/nginx/conf.d/default.conf
+echo "    proxy_set_header Upgrade \$http_upgrade;" >> /etc/nginx/conf.d/default.conf
+echo "    proxy_set_header Connection \$http_connection;" >> /etc/nginx/conf.d/default.conf
 echo "  }" >> /etc/nginx/conf.d/default.conf
 fi
 
